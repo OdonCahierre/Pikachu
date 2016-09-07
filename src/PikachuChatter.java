fadf540f09dcd487bb11c79e8f4d392b6b1bd709
OdonCahierre <windward_student@school.windwardschool.org>
2016-09-07 09:39:13 -0700

    Pikachu

---
+++ src/PikachuChatter.java
@@ -0,0 +1,17 @@
+
+public class PikachuChatter {
+	//The code will print out Pikachu twice saying a different saying each time
+public static void Pikachu(){
+	
+	System.out.println(" (\\_/)");
+	System.out.println(" (O^.^)");
+	System.out.println("z(_(\")(\") \n\n");
+	
+}
+public static void main(String[] args){
+	System.out.println("        Pika pika pika chu pika chu peeee ka pika chu!");
+	Pikachu();
+	System.out.println("        Pika? Pika pika pika chu peeeee ka chu!");
+	Pikachu();
+}
+}
